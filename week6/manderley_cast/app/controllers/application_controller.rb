8f849fa61613d7e8ff4196d40e29ae79c78aa2c6
Mauro <mauro_gs@hotmail.com>
2014-12-06 10:33:13 +0100

    Fixed edit people

--- week6/manderley_cast/app/controllers/application_controller.rb
+++ week6/manderley_cast/app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def set_locale
-      I18n.locale = params[:locale]
+      I18n.locale = params[:locale] || I18n.default_locale
   end
 
   def default_url_options options = {}
